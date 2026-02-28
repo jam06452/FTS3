2c3ed80028694d5a6eb0a1966f6693e10bd726c8
jam06452 <jam@jam06452.uk>
2026-02-28 11:10:11 +0000

    S3 lib

---
+++ lib/fts3/S3.ex
@@ -0,0 +1,27 @@
+defmodule S3 do
+
+  def client(base_url, region, access_key_id, secret_access_key) do
+    Req.new(
+      base_url: base_url,
+      aws_sigv4: [
+        service: :s3,
+        region: region,
+        access_key_id: access_key_id,
+        secret_access_key: secret_access_key
+      ],
+      decode_body: true
+    )
+  end
+
+  def get_bucket_info(client, bucket) do
+    client |> Req.get(url: "/#{bucket}")
+  end
+
+  def get_object(client, bucket, key) do
+    client |> Req.get(url: "/#{bucket}/#{key}")
+  end
+
+  def put_object(client, bucket, key, body) do
+    client |> Req.put(url: "/#{bucket}/#{key}", body: body)
+  end
+end
